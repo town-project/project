1a8902214bbcb4f64bb25f3c8ec4efd528b144a0
umin <ymn0529@gmail.com>
2023-08-22 03:19:02 +0900

    [2023-08-22][umin] 로그인, 회원가입 페이지 mui 적용 및 헤더 푸터 추가

--- src/frontEnd/Components/Login/LoginForm.tsx
+++ src/frontEnd/Components/Login/LoginForm.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import styles from "./LoginForm.module.css";
+import { TextField, Button, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
+import Header from "../../Common/Header";
+import Footer from "../../Common/Footer";
 export default function LoginForm() {
   // 페이지를 이동할 네비게이트 함수
   const navigate = useNavigate();
@@ -12,25 +13,85 @@ export default function LoginForm() {
   }
 
   return (
-    <div className={styles.formBox}>
-      <h2>로그인</h2>
-      <div className={styles.inputSection}>
-        <input className={styles.inputValue} placeholder="ID" />
-      </div>
-      <div className={styles.inputSection}>
-        <input className={styles.inputValue} placeholder="Password" />
-      </div>
-      <div className={styles.inputSection}>
-        <button className={styles.sendBtn}>로그인</button>
-        <button className={styles.sendBtn} onClick={goBack}>
-          취소
-        </button>
-      </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Header />
+      <Box
+        sx={{
+          maxWidth: "300px",
+          margin: "100px auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>로그인</h2>
+        <TextField
+          placeholder="ID"
+          // variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              borderRadius: 70,
+              paddingLeft: 20,
+              height: 50,
+            },
+          }}
+        />
+        <TextField
+          placeholder="Password"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              borderRadius: 70,
+              paddingLeft: 20,
+              height: 50,
+            },
+          }}
+        />
 
-      <div className={styles.accountOptionsBox}>
-        <span className={styles.accountOptions}> 아이디 찾기</span>/
-        <span className={styles.accountOptions}>비밀번호 찾기</span>
-      </div>
-    </div>
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{ borderRadius: 70, marginRight: 10 }}
+          style={{
+            height: 50,
+            borderRadius: 70,
+            marginTop: 15,
+            fontSize: 16,
+            backgroundColor: "black",
+            color: "white",
+          }}
+        >
+          로그인
+        </Button>
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={goBack}
+          sx={{ borderRadius: 70 }}
+          style={{
+            height: 50,
+            borderRadius: 70,
+            marginTop: 15,
+            fontSize: 16,
+            backgroundColor: "black",
+            color: "white",
+          }}
+        >
+          취소
+        </Button>
+        <div style={{ marginTop: 50 }}>
+          <span style={{ fontSize: 14, margin: "0 30px", cursor: "pointer" }}>
+            아이디 찾기
+          </span>
+          |
+          <span style={{ fontSize: 14, margin: "0 30px", cursor: "pointer" }}>
+            비밀번호 찾기
+          </span>
+        </div>
+      </Box>
+      <Footer />
+    </Box>
   );
 }
--- src/frontEnd/Components/Login/RegisterForm.tsx
+++ src/frontEnd/Components/Login/RegisterForm.tsx
@@ -1,41 +1,27 @@
 import React from "react";
-import styles from "./RegisterForm.module.css";
+import { Box, Button, TextField } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import type { RootState } from "../../Redux/store/store";
-import { setPassWord, setPassWordCheck } from "../../Redux/slice/LoginSlice";
-import { useEffect } from "react";
-import Alert from "@mui/material/Alert";
 
-export default function RegisterForm() {
-  const navigate = useNavigate(); // 페이지를 이동할 네비게이트 함수
-  
-  // 비밀번호
-  const pw: string = useSelector( 
-    (state: RootState) => state.LoginReducer.password
-  );
+import Header from "../../Common/Header";
+import Footer from "../../Common/Footer";
 
-  // 비밀번호 확인  
-  const pwCheck: string = useSelector(
-    (state: RootState) => state.LoginReducer.passwordCheck
-  );
+// 핸드폰, 인증 버튼 높낮이 맞추기 위해 생성한 theme
+const theme = createTheme({
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          marginBottom: "16px",
+        },
+      },
+    },
+  },
+});
 
-  // 비밀번호 변경
-  const pwDispatch = useDispatch();
-
-  // 비밀번호 변경 함수
-  function pwChange(e: React.ChangeEvent<HTMLInputElement>, id: number): void {
-    // 비밀번호 상태 변경
-    if (id === 1) pwDispatch(setPassWord(e.target.value));
-    // 비밀번호 확인 상태 변경
-    else pwDispatch(setPassWordCheck(e.target.value));
-  }
-
-  // 비밀번호 일치여부 확인
-  function pwSame(): boolean {
-    if (pw === pwCheck && pw.length >= 8 && pwCheck.length >= 8) return true;
-    else return false;
-  }
+const RegisterForm: React.FC = () => {
+  // 페이지를 이동할 네비게이트 함수
+  const navigate = useNavigate();
 
   // 메인으로 돌아가는 함수
   function goBack(): void {
@@ -43,64 +29,162 @@ export default function RegisterForm() {
   }
 
   return (
-    <div className={styles.formBox}>
-      <h2>회원가입</h2>
-
-      <div className={styles.inputSection}>
-        <div className={styles.inputTitle}>아이디</div>
-        <input className={styles.inputValue} placeholder="" />
-      </div>
-
-      <div className={styles.inputSection}>
-        <div className={styles.inputTitle}>비밀번호</div>
-        <input
-          className={styles.inputValue}
-          placeholder="8~16자 영문 대소문자, 숫자, 특수문자"
-          onChange={(e) => pwChange(e, 1)}
-        />
-      </div>
-
-      <div className={styles.inputSection}>
-        <div className={styles.inputTitle}>비밀번호 확인</div>
-        <input
-          className={styles.inputValue}
+    <Box sx={{ flexGrow: 1 }}>
+      <Header />
+      <Box sx={{ maxWidth: "300px", margin: "30px auto", textAlign: "center" }}>
+        <h2>회원가입</h2>
+        <TextField
+          label="아이디"
           placeholder=""
-          onChange={(e) => pwChange(e, 2)}
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              borderRadius: 70,
+              height: 50,
+            },
+            classes: {
+              focused: "focused",
+            },
+          }}
+          //label 글자에 여백을 줌
+          InputLabelProps={{
+            style: {
+              marginLeft: 15,
+            },
+          }}
         />
-      </div>
-
-      <div className={styles.inputSection}>
-        <div className={styles.inputTitle}>닉네임</div>
-        <input className={styles.inputValue} placeholder={``} />
-      </div>
-
-      <div className={styles.inputSection}>
-        <div className={styles.inputTitle}>핸드폰</div>
-        <input
-          className={styles.inputValue}
-          placeholder={`"-"를 제외하고 입력`}
-        ></input>
-        <button className={styles.AuthPhone}>인증</button>
-      </div>
-
-      {/* <div className={styles.btnGroup}> */}
-      <div className={styles.inputSection}>
-        <button
-          className={styles.RegisterBtn}
-          onClick={() => { // 비밀번호와 비밀번호 확인이 서로 일치하는지 확인
-            if (pwSame()) alert('회원 가입 성공')
-            else alert('비밀번호가 8자리 이하이거나 서로 일치하지 않습니다.')
+        <TextField
+          label="비밀번호"
+          placeholder="8~16자 영문 대소문자, 숫자, 특수문자"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              borderRadius: 70,
+              height: 50,
+            },
+          }}
+          //label 글자에 여백을 줌
+          InputLabelProps={{
+            style: {
+              marginLeft: 15,
+            },
+          }}
+        />
+        <TextField
+          label="비밀번호 확인"
+          placeholder=""
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              borderRadius: 70,
+              height: 50,
+            },
+          }}
+          //label 글자에 여백을 줌
+          InputLabelProps={{
+            style: {
+              marginLeft: 15,
+            },
+          }}
+        />
+        <TextField
+          label="닉네임"
+          placeholder=""
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          InputProps={{
+            style: {
+              borderRadius: 70,
+              height: 50,
+            },
+          }}
+          //label 글자에 여백을 줌
+          InputLabelProps={{
+            style: {
+              marginLeft: 15,
+            },
+          }}
+        />
+        <ThemeProvider theme={theme}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <TextField
+              label="핸드폰"
+              placeholder='"-"를 제외하고 입력'
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              InputProps={{
+                style: {
+                  borderRadius: 70,
+                  height: 50,
+                },
+              }}
+              //label 글자에 여백을 줌
+              InputLabelProps={{
+                style: {
+                  marginLeft: 15,
+                },
+              }}
+            />
+            <Button
+              className="AuthPhone"
+              style={{
+                borderRadius: 30,
+                height: 50,
+                backgroundColor: "black",
+                color: "white",
+              }}
+            >
+              인증
+            </Button>
+          </Box>
+        </ThemeProvider>
+        {/* 회원가입버튼 */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Button
+            className="RegisterBtn"
+            fullWidth
+            variant="contained"
+            style={{
+              height: 50,
+              borderRadius: 70,
+              marginTop: 15,
+              fontSize: 16,
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            회 원 가 입
+          </Button>
+        </Box>
+        {/* 취소버튼 */}
+        <Button
+          className="cancleBtn"
+          fullWidth
+          variant="outlined"
+          onClick={goBack}
+          style={{
+            height: 50,
+            borderRadius: 70,
+            marginTop: 20,
+            fontSize: 16,
+            backgroundColor: "black",
+            color: "white",
           }}
         >
-          회원가입
-        </button>
-      </div>
-      <div className={styles.inputSection}>
-        <button className={styles.cancleBtn} onClick={goBack}>
-          취소
-        </button>
-      </div>
-      {/* </div> */}
-    </div>
+          취 소
+        </Button>
+      </Box>
+      <Footer />
+    </Box>
   );
-}
+};
+
+export default RegisterForm;
