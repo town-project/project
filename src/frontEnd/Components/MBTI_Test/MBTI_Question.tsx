639068905c668e11ad77af10b1195109629176b8
choi-ik <ci980704@naver.com>
2023-09-13 01:21:24 +0900

    [2023-09-13][choi-ik] Feat: 다음 질문 넘어가기, 답변 선택시 버튼 배경색 변경 기능, 뒤로가기 기능 다음 질문으로 넘어가기 위해 답변 선택 답변 선택 시 버튼 배경색 변경 -> 다음 질문으로 넘어가면 배경색 다시 초기화 뒤로가기 버튼 클릭 시 이전 화면인 테스트 준비 화면으로 페이지 전환

--- src/frontEnd/Components/MBTI_Test/MBTI_Question.tsx
+++ src/frontEnd/Components/MBTI_Test/MBTI_Question.tsx
@@ -5,8 +5,12 @@ import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../../Redux/store/store";
 
 import { Box, Button, Container, Paper, styled } from "@mui/material";
-import { setCurrentNum } from "../../Redux/slice/MBTI_TestSlice";
-import { useEffect } from "react";
+import {
+  setAnswer,
+  setChangeAnswer,
+  setCurrentNum,
+} from "../../Redux/slice/MBTI_TestSlice";
+import { useEffect, useState } from "react";
 
 // 질문지 Paper
 const Item = styled(Paper)(({ theme }) => ({
@@ -19,6 +23,10 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function MBTI_Question() {
+  // 버튼 배경색 상태
+  const [ansColor, setAnsColor] = useState([false, false]);
+  const navigate = useNavigate();
+
   // 질문지 배열
   const question: string[] = useSelector(
     (state: RootState) => state.MBTI_TestReducer.MBTI_Question.question
@@ -27,9 +35,14 @@ export default function MBTI_Question() {
   const currentNum: number = useSelector(
     (state: RootState) => state.MBTI_TestReducer.MBTI_Question.currentNum
   );
+  // 답변 여부 배열
+  const answer: boolean[][] = useSelector(
+    (state: RootState) => state.MBTI_TestReducer.MBTI_Question.answer
+  );
 
-  // 질문지의 질문이 전환될 수 있게 인덱스의 값을 변경 할 Dispatch
-  const currentNumDispatch = useDispatch();
+  const currentNumDispatch = useDispatch(); // 질문지의 질문이 전환될 수 있게 인덱스의 값을 변경 할 Dispatch
+  const answerDispatch = useDispatch(); // 질문 배열의 길이만큼 답변 배열 초기화 하기 위한 dispatch
+  const changeAnswerDispatch = useDispatch(); // 질문에 대한 답변 여부 업데이트 할 dispatch
 
   // 질문지 배열의 인덱스 증가
   function increaseNum(n: number): void {
@@ -38,10 +51,22 @@ export default function MBTI_Question() {
       : currentNumDispatch(setCurrentNum(currentNum - 1));
   }
 
+  // 답변 여부 업데이트
+  function changeAnswer(value: [number, boolean]): void {
+    changeAnswerDispatch(setChangeAnswer(value));
+  }
+  
+  // 페이지 이동 함수
+  function movePage(page: string): void {
+    navigate(`/${page}`);
+  }
+
   useEffect(() => {
     // 렌더링 될 때마다 항상 0으로 초기화
-    currentNumDispatch(setCurrentNum(0));    
-  }, [])
+    currentNumDispatch(setCurrentNum(0));
+    // 컴포넌트가 마운트 되면 질문 배열의 길이 만큼 답변 선택 배열 생성
+    answerDispatch(setAnswer(question.length));
+  }, []);
 
   return (
     <Box>
@@ -51,8 +76,29 @@ export default function MBTI_Question() {
           <Box>{question[currentNum]}</Box>
           <Box>
             <Button
-              style={{ borderColor: "gray", borderRadius: "20px" }}
+              style={
+                ansColor[0]
+                  ? {
+                      borderColor: "gray",
+                      borderRadius: "20px",
+                      backgroundColor: "gray",
+                    }
+                  : {
+                      borderColor: "gray",
+                      borderRadius: "20px",
+                      backgroundColor: "white",
+                    }
+              }
               variant="outlined"
+              onClick={() => {
+                // 질문에 대한 답변 배열의 답변 여부 변경
+                changeAnswer([currentNum, true]);
+                // 클릭된 버튼 색상 바꾸기 위해 ansColor의 bool값 업데이트
+                let temp = ansColor;
+                temp[0] = true;
+                temp[1] = !temp[0];
+                setAnsColor([...temp]);
+              }}
               size="medium"
             >
               {"답변 1"}
@@ -60,7 +106,28 @@ export default function MBTI_Question() {
           </Box>
           <Box>
             <Button
-              style={{ borderColor: "gray", borderRadius: "20px" }}
+              style={
+                ansColor[1]
+                  ? {
+                      borderColor: "gray",
+                      borderRadius: "20px",
+                      backgroundColor: "gray",
+                    }
+                  : {
+                      borderColor: "gray",
+                      borderRadius: "20px",
+                      backgroundColor: "white",
+                    }
+              }
+              onClick={() => {
+                // 질문에 대한 답변 배열의 답변 여부 변경
+                changeAnswer([currentNum, false]);
+                // 클릭된 버튼 색상 바꾸기 위해 ansColor의 bool값 업데이트
+                let temp = ansColor;
+                temp[1] = true;
+                temp[0] = !temp[1];
+                setAnsColor([...temp]);
+              }}
               variant="outlined"
               size="medium"
             >
@@ -69,17 +136,24 @@ export default function MBTI_Question() {
           </Box>
         </Item>
         <Box style={{ justifyContent: "space-between" }}>
-          <Button
-            style={{ marginTop: "5vh", float: 'left', borderColor: "gray" }}
-            onClick={() => increaseNum(-1)}
+        <Button
+            style={{ marginTop: "5vh", float: "left", borderColor: "gray" }}
+            onClick={() => {
+              if (answer[currentNum][2]) increaseNum(1); // 현재 질문에 대한 답변을 골랐을 때만 다음 질문으로 넘어 갈 수 있음
+              setAnsColor([false, false]); // 다음 질문으로 넘어갈 시, 버튼 색상 초기화
+              movePage('MBTI_Test');
+            }}
             variant="outlined"
             size="medium"
           >
-            {"<"}
+            {"뒤로 가기"}
           </Button>
           <Button
-            style={{ marginTop: "5vh", float: 'right', borderColor: "gray" }}
-            onClick={() => increaseNum(1)}
+            style={{ marginTop: "5vh", float: "right", borderColor: "gray" }}
+            onClick={() => {
+              if (answer[currentNum][2]) increaseNum(1); // 현재 질문에 대한 답변을 골랐을 때만 다음 질문으로 넘어 갈 수 있음
+              setAnsColor([false, false]); // 다음 질문으로 넘어갈 시, 버튼 색상 초기화
+            }}
             variant="outlined"
             size="medium"
           >
--- src/frontEnd/Redux/slice/MBTI_TestSlice.ts
+++ src/frontEnd/Redux/slice/MBTI_TestSlice.ts
@@ -2,40 +2,57 @@ import { createSlice } from "@reduxjs/toolkit";
 
 /* initialState의 기본 타입을 인터페이스로 지정 */
 interface MBTI_TestState {
-   MBTI_Question: {
-    question: string[];
-    currentNum: number;
-   }
+  MBTI_Question: {
+    question: string[]; // 질문 리스트 
+    currentNum: number; // 질문 인덱스
+    answer: boolean[][]; // 답변 여부 리스트
+  };
 }
 
 const initialState: MBTI_TestState = {
-    MBTI_Question: {
-        question: ["약속이 생겼다 나는", 
-        "당신은 사람들과 어울리는 것을 좋아하나요, 아니면 혼자 있는 것을 더 좋아하나요?", 
-        "긴 대화를 나누는 것이 좋은가요, 아니면 길게 말하는 것을 피하고 짧은 답변을 좋아하나요?"
-        ],
-        currentNum: 0,
-    }
-}
+  MBTI_Question: {
+    question: [
+      "약속이 생겼다 나는",
+      "당신은 사람들과 어울리는 것을 좋아하나요, 아니면 혼자 있는 것을 더 좋아하나요?",
+      "긴 대화를 나누는 것이 좋은가요, 아니면 길게 말하는 것을 피하고 짧은 답변을 좋아하나요?",
+    ],
+    currentNum: 0,
+    answer: [[]],
+  },
+};
 
 export const MBTI_TestSlice = createSlice({
-    name: 'MBTI_TestSlice', // slice 이름
-    initialState, // 전역으로 관리할 상태의 초기값
-    reducers: { // 상태를 변경시키는 리듀서 함수
+  name: "MBTI_TestSlice", // slice 이름
+  initialState, // 전역으로 관리할 상태의 초기값
+  reducers: {
+    // 상태를 변경시키는 리듀서 함수
 
-        // 질문 목록 서버에서 받아와 저장
-        setQuestion: (state, action) => {
-            state.MBTI_Question.question = action.payload;
-        },
+    // 질문 목록 서버에서 받아와 저장
+    setQuestion: (state, action) => {
+      state.MBTI_Question.question = action.payload;
+    },
 
-        // 질문 목록 보여줄 인덱스
-        setCurrentNum: (state, action) => {
-            // 배열의 범위를 벗어나지 않게 하기 위함
-            if (action.payload >= 0 && action.payload < state.MBTI_Question.question.length) state.MBTI_Question.currentNum = action.payload;
-            
-        }
+    // 질문 목록 보여줄 인덱스
+    setCurrentNum: (state, action) => {
+      // 배열의 범위를 벗어나지 않게 하기 위함
+      if (
+        action.payload >= 0 &&
+        action.payload < state.MBTI_Question.question.length
+      )
+        state.MBTI_Question.currentNum = action.payload;
+    },
+    // 답안 배열을 질문 배열의 길이 만큼 초기화
+    setAnswer: (state, action) => {
+        state.MBTI_Question.answer = Array.from({length: action.payload}, () => [false, false, false]); 
+    },
+    // 답안 배열 bool 값 업데이트
+    setChangeAnswer: (state, action) => {
+        state.MBTI_Question.answer[action.payload[0]][0] = action.payload[1];
+        state.MBTI_Question.answer[action.payload[0]][1] = !action.payload[1];
+        state.MBTI_Question.answer[action.payload[0]][2] = true;
     }
-})
+  },
+});
 
-export const {setQuestion, setCurrentNum} = MBTI_TestSlice.actions
-export default MBTI_TestSlice.reducer
+export const { setQuestion, setCurrentNum, setAnswer, setChangeAnswer } = MBTI_TestSlice.actions;
+export default MBTI_TestSlice.reducer;
